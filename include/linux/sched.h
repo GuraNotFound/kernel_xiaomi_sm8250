2d9874db39b6966ef8d0cfd1b7ff43b2d09f2f09
TIMISONG-dev <dima.pilaer@gmail.com>
2025-12-22 19:17:59 +0300

    sched: Finally remove task boost feature

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -797,11 +797,6 @@ struct task_struct {
 	const struct sched_class	*sched_class;
 	struct sched_entity		se;
 	struct sched_rt_entity		rt;
-	u64				 last_sleep_ts;
-
-	int				boost;
-	u64				boost_period;
-	u64				boost_expires;
 	struct sched_dl_entity		dl;
 
 #ifdef CONFIG_SCHED_CORE
